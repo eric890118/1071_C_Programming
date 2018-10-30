aee0fc93c3ed144fc40d365a6090e0f47ba56d40
eric890118 <43335682+eric890118@users.noreply.github.com>
2018-10-30 17:14:50 +0800

    Create test_p4.cpp

---
+++ w08/test_p4.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+#define Max 100
+
+int main(){
+    int s;
+    int num=-1,fail=0,pass=0;
+    float aver=0,add=0;
+
+    printf("Enter a series of scores between 0~100(-1 to exit).\n");
+    for(;s!=-1;num++){
+        scanf("%d",&s);
+        if(s<=100&&s>=60)
+            pass=pass+1;
+        if(s<60&&s>=0)
+            fail=fail+1;
+        if(s<=100&&s>=0)
+            add=add+s;
+    }
+    aver=add/num;
+
+    printf("Scores listing:\n");
+    printf("%d\n\n",s);
+
+    printf("The summary:\n");
+    printf("NumberCount = %d\n",num);
+    printf("PassCount = %d\n",pass);
+    printf("FailedCount = %d\n",fail);
+    printf("Average= %.1f\n",aver);
+}
