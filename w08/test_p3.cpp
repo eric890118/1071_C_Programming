e15f9491d6be7c19e6877580aa218d49a728c75e
eric890118 <43335682+eric890118@users.noreply.github.com>
2018-10-30 17:50:42 +0800

    Update test_p3.cpp

--- w08/test_p3.cpp
+++ w08/test_p3.cpp
@@ -1,36 +1,61 @@
 #include<stdio.h>
-#include<ctype.h>
 #include<math.h>
+#define Max 100
 
-int checkSqrt(int n)
-{
-    int a;
-    a=sqrt(n);
-    if(a*a==n)
-        return n;
-    else
-        return 1;
-}
+int main(){
+    int x,s[Max];
+    int num=-1,fail=0,pass=0;
+    float aver=0,add=0;
+    int a=0,b=0,c=0,d=0,e=0,f=0;
 
-int main()
-{
-    int n,a;
-    char cont='Y';
-    while(cont=='Y')
-    {
-        printf("Enter n : ");
-        scanf("%d",&n);
-        a=sqrt(n);
-        if(n==checkSqrt(n))
-            printf("Yes. %d = %d x %d\n",n,a,a);
-        else
-            printf("No.\n");
+    printf("Enter a series of scores between 0~100(-1 to exit).\n");
+    for(int i=0;x!=-1;num++){
+        scanf("%d",&x);
+        s[i]=x;
+        i+=1;
+        if(x<=100&&x>=60)
+            pass=pass+1;
+        if(x<60&&x>=0)
+            fail=fail+1;
+        if(x<=100&&x>=0)
+            add=add+x;
 
-            printf("Continue (Y/N)?  ");
-        getchar();
-        cont=toupper(getchar());
-        printf("\n");
+        if(x==100) a=a+1;
+        if(x<100&&x>=90) b=b+1;
+        if(x<90&&x>=80) c=c+1;
+        if(x<80&&x>=70) d=d+1;
+        if(x<70&&x>=60) e=e+1;
+        if(x<60&&x>=0) f=f+1;
     }
-    printf("Coding by 407411015\n");
+    aver=add/num;
 
+    printf("\nScores listing:\n");
+    for(int i=0;i<=num;i++){
+        if(s[i]>=0&&s[i]<=100)
+            printf("%d ",s[i]);
+    }
+
+    printf("\n");
+    printf("The summary:\n");
+    printf("NumberCount = %d\n",num);
+    printf("PassCount = %d\n",pass);
+    printf("FailedCount = %d\n",fail);
+    printf("Average= %.1f\n",aver);
+
+    printf("\n100   %d ",a);
+    for(;a>0;a--) printf("*");
+    printf("\n90~99 %d ",b);
+    for(;b>0;b--) printf("*");
+    printf("\n80~89 %d ",c);
+    for(;c>0;c--) printf("*");
+    printf("\n70~79 %d ",d);
+    for(;d>0;d--) printf("*");
+    printf("\n60~69 %d ",e);
+    for(;e>0;e--) printf("*");
+    printf("\n0~59  %d ",f);
+    for(;f>0;f--) printf("*");
+
+    printf("\n\n");
+
+    printf("Coding by 407411015\n");
 }
