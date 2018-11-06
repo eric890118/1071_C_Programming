ec137e7a0bb9b6db8dcbded212d5490d3469b6e9
eric890118 <43335682+eric890118@users.noreply.github.com>
2018-11-06 16:38:25 +0800

    Update rand-date.cpp

--- w09/rand-date.cpp
+++ w09/rand-date.cpp
@@ -17,7 +17,7 @@ int main(){
     scanf("%d",&n);
     for(int i=0;i<n;i++){
         dates[i].year = rand()%11+2010;
-		dates[i].month = rand()%11+1;
+		dates[i].month = rand()%12+1;
 		dates[i].day = rand()%31+1;
 
         printf("%d/%02d/%02d\n",dates[i].year,dates[i].month,dates[i].day);
